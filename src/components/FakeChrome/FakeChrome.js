a77ef9559a9145a8daec905096b5d89c65a7aa2d
clarencenpy <clarencenpy@gmail.com>
2016-09-28 12:27:18 -0400

    added bookmarks to fake chrome

--- src/components/FakeChrome/FakeChrome.js
+++ src/components/FakeChrome/FakeChrome.js
@@ -3,12 +3,18 @@ import s from './FakeChrome.scss'
 import left from './assets/LeftChrome.jpg'
 import right from './assets/RightChrome.jpg'
 import center from './assets/CenterChrome.jpg'
+import activeIcon from './assets/Icon-active.png'
+import inactiveIcon from './assets/Icon-inactive.png'
 
 export const FakeChrome = (props) => (
   <div>
     <div className={s.url}>{props.url}</div>
     <div className={s.title}>{props.title}</div>
-    <div className={s.iconClickTarget} onClick={props.clickLumiHandler}></div>
+    <div className={s.iconClickTarget} onClick={props.clickLumiHandler}>
+      <img className={s.icon} src={inactiveIcon} />
+    </div>
+    <div className={s.fbClickTarget}></div>
+    <div className={s.gClickTarget}></div>
     <img src={left} className={s.left} />
     <img src={center} className={s.center} />
     <img src={right} className={s.right} />
